3d35db25e769ab39197726a3fa0379aea42fde69
Rufheart <rufetnuriyev.nr@gmail.com>
2022-12-21 00:09:16 +0400

    f-1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Chat,{Message, Card} from './components/Chat';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+        <Route path='' element={<Chat/>}>
+          <Route path='messages/:userName/:id' element={<Message />} />
+        </Route>
+        <Route path='/card' element={<Card/>}/>
+    </Routes>
   );
 }
 
---
+++ src/components/Chat.jsx
@@ -0,0 +1,72 @@
+import './Chat.css';
+import { NavLink } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { useRef, useState } from 'react';
+
+function Chat(){
+    let num1 = '1'
+    let [show, setShow] = useState(true);
+    let nav = useRef()
+    function showNav(){
+        if(show){
+            setShow(!show)
+            nav.current.style.left = '0'
+        }else{
+            setShow(!show)
+            console.log('asasa', nav)
+            nav.current.style.left = '-40%'
+        }
+        }   
+    return(
+        <div className="chat">
+            <nav ref={nav}>
+                <ul>
+                    <li><NavLink to={`/messages/muxtar/${num1}`}>Muxtar</NavLink> </li>
+                    <li><NavLink to={`/messages/rufet/${num1}`}>Rufet</NavLink> </li>
+                </ul>
+                <div>
+                    <li><NavLink to='/card'>Card</NavLink></li>
+                    <li><NavLink>About</NavLink></li>
+                    <li><NavLink></NavLink></li>
+                </div>
+            </nav>
+            
+            <div className="messages-chat">
+                <div className="welcome">
+                    welecome 
+                </div>
+                <Outlet />
+            </div>
+            <div className="show" onClick={showNav}>show</div>
+
+        </div>
+    )
+}
+
+
+function Message(){
+    console.log('message isledi')
+    let params = useParams();
+    return(
+        <div className="main-message">
+             <div className="header">Header 
+             </div>
+            <div className="messages">{params.userName}</div>
+        </div>
+     
+    )
+}
+
+
+function Card(){
+    return(
+        <div>
+            Cards
+        </div>
+    )
+}
+
+
+export default Chat;
+export {Message, Card};
