f2ce3c858f917d6b1030f8bbb80fb3f65dcba59c
Rufheart <rufetnuriyev.nr@gmail.com>
2022-12-21 23:28:34 +0400

    f-3

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,15 +3,17 @@ import { NavLink } from 'react-router-dom';
 
 function Cards(){
     return(
-        <div>
-            <div className='cards'>
+        <div className='cards'>
+            <div className='card'>
                 Cards
             </div>
-            <ul>
-                <li><NavLink to='/'>Chat</NavLink></li>
-                <li><NavLink>About</NavLink></li>
-                <li><NavLink>Contact</NavLink></li>
-            </ul>
+            <div>
+                <ul>
+                    <li><NavLink to='/'>Chat</NavLink></li>
+                    <li><NavLink>About</NavLink></li>
+                    <li><NavLink>Contact</NavLink></li>
+                </ul>
+            </div>
         </div>
 
     )
