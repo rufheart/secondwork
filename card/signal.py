905ef014b6fdb4befd0dc67aea015161dfab93a9
Rufheart <rufetnuriyev.nr@gmail.com>
2023-02-03 01:43:31 +0400

    f-36

--- card/signal.py
+++ card/signal.py
@@ -5,6 +5,7 @@ from django.dispatch import receiver
 foreignkeys = []
 all = ['Phone','Home','Work','Car','Tiktok','Facebook','Instagram']
 lists = ['Phone','Home','Work','Car','Tiktok','Facebook','Instagram']
+Json = {'Phone':'ph_numbers_card','Home':'card_home','Work':'card_work','Car':'card_cars','Tiktok':'card_tiktok','Facebook':'card_facebook','Instagram':'card_instagram'}
 @receiver(post_save)
 def created_card(sender, instance ,created=False, **kwargs):
     if sender.__name__=='Card_Main':
@@ -40,63 +41,54 @@ def created_card(sender, instance ,created=False, **kwargs):
         elif card.features == None:
             about.features = False
             about.save() 
-    count = 0
     for one in lists:
         if sender.__name__==one:
-            # sd = sender.objects.get(id=instance.id)
-            print(instance[0])
-            if one in all:
+            alfa = sender.objects.get(id=instance.id)
+            if one == 'Phone':
+                about = About.objects.get(card_about=alfa.ph_numbers_card)
+                about.phone
+                about.save()
+                if one in all:
+                    all.remove(one)                
+            if one == 'Work':
+                about = About.objects.get(card_about=alfa.card_work)
+                print(about.__new__)
+                about.work=True
+                about.save()  
+                if one in all:
+                    all.remove(one) 
+            if one == 'Home':
+                about = About.objects.get(card_about=alfa.card_home)
+                about.home=True
+                about.save()  
+                if one in all:
+                    all.remove(one)
+            if one == 'Car':
+                about = About.objects.get(card_about=alfa.card_cars)
+                about.car=True
+                about.save()   
+                if one in all:
+                    all.remove(one)  
+            if one == 'Tiktok':
+                about = About.objects.get(card_about=alfa.card_tiktok)
+                about.tiktok = True
+                about.save()
+                if one in all:
+                    all.remove(one)
+            if one == 'Facebook':
+                about = About.objects.get(card_about=alfa.card_facebook)
+                about.facebook = True
+                about.save()  
+                if one in all:
+                    all.remove(one)
+            if one == 'Instagram':
+                about = About.objects.get(card_about=alfa.card_instagram)
+                about.instagram = True
+                about.save() 
+                if one in all:
+                    all.remove(one)   
 
-                all.remove(one)
-    # if sender.__name__== 'Phone':
-    #     phone = sender.objects.get(id=instance.id)
-    #     about = About.objects.get(card_about=phone.ph_numbers_card)
-    #     about.phone = True
-    #     about.save()
-    #     if 'Phone' in all:
-    #         all.remove('Phone')
-    # if sender.__name__=='Home':
-    #     home = sender.objects.get(id=instance.id)
-    #     about = About.objects.get(card_about=home.card_home)
-    #     about.home = True
-    #     about.save()
-    #     if 'Home' in all:
-    #         all.remove('Home')
-    # if sender.__name__=='Work':
-    #     work = sender.objects.get(id=instance.id)
-    #     about = About.objects.get(card_about=work.card_work)
-    #     about.work = True
-    #     about.save()
-    #     if 'Work' in all:
-    #         all.remove('Work')
-    # if sender.__name__=='Car':
-    #     car = sender.objects.get(id=instance.id)
-    #     about = About.objects.get(card_about=car.card_cars)  
-    #     about.car = True
-    #     about.save() 
-    #     if 'Car' in all: 
-    #         all.remove('Car')
-    # if sender.__name__=='Tiktok':
-    #     tiktok = sender.objects.get(id=instance.id)
-    #     about = About.objects.get(card_about=tiktok.card_tiktok)  
-    #     about.tiktok = True
-    #     about.save() 
-    #     if 'Tiktok' in all: 
-    #         all.remove('Tiktok')      
-    # if sender.__name__=='Facebook':
-    #     facebook = sender.objects.get(id=instance.id)
-    #     about = About.objects.get(card_about=facebook.card_facebook)  
-    #     about.facebook = True
-    #     about.save() 
-    #     if 'Facebook' in all: 
-    #         all.remove('Facebook')  
-    # if sender.__name__=='Instagram':
-    #     instagram = sender.objects.get(id=instance.id)
-    #     about = About.objects.get(card_about=instagram.card_instagram)  
-    #     about.instagram = True
-    #     about.save() 
-    #     if 'Instagram' in all: 
-    #         all.remove('Instagram')                          
+                       
     if sender.__name__=='Card_Main':
         for one in all:
             if one == 'Phone':
@@ -133,4 +125,4 @@ def created_card(sender, instance ,created=False, **kwargs):
                 about = About.objects.get(card_about = instance)   
                 about.instagram = False 
                 about.save()                                                        
-    print(all)    
+       
