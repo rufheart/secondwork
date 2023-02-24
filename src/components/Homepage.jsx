90f052ced3196c6020baddef7bf952838b5ce4e2
Rno Rno <rnorno@gmail.com>
2023-02-24 16:42:46 +0400

    f-46

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -10,15 +10,18 @@ import { Context } from './Context';
 function Homepage(){
     return(
         <div className='main'>
-            <div className='left-bar'>
+            <nav className='left-bar'>
                 <div className='search-bar'>
                     <div className='menu-icon'><div className='menu-icon2'> <i className='fa fa-bars'/></div></div>
-                    <div className='frame-search'><i className='fa fa-search'/><input type="text" /></div>
+                    <div className='frame-search'><div><i className='fa fa-search'/></div><input type="text" placeholder="Search" /></div>
                 </div>
                 <div className='chat-list'>
                     
                 </div>
-            </div>
+                <div className='tab-bar-menu'>
+                    <div>soz</div>
+                </div>
+            </nav>
         </div>
     )
 }
