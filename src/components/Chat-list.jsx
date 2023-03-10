45adaa377a4b039a9e9189c6955b6a7ab622cdaa
Rno Rno <rnorno@gmail.com>
2023-03-10 15:54:44 +0400

    f-59

--- src/components/Chat-list.jsx
+++ src/components/Chat-list.jsx
@@ -2,7 +2,7 @@ import './Chat-list.css';
 import { NavLink } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
-import { useRef, useState } from 'react';
+import { useRef, useState,useEffect} from 'react';
 import { useContext } from 'react';
 import { Context } from './Context';
 
@@ -12,24 +12,31 @@ function Chat(){
     let [activecolor, setAcColor] = useState()
     let [showmenu, setShowMenu] = useState(false)
     let [newcontact, setNewContact] = useState(false)
-    function ShowMenu(){
-        setShowMenu(!showmenu)
-    }
-    function ShowNewContact(){
-        setNewContact(!newcontact)
-        setShowMenu(false)
-    }
+    let menuRef=useRef();
+    useEffect(()=>{
+        let handler = (e)=>{
+            if(!menuRef.current.contains(e.target)){
+                setShowMenu(false)
+                setNewContact(false)
+            }
+        };
+        document.addEventListener("mousedown",handler)
+
+        return()=>{
+            document.removeEventListener("mousedown", handler)
+        }
+    });
     return(
         <div className='frame'>
-            <div className='search-bar'>
-                <div className='menu-icon'><div className='menu-icon2'> <i className='fa fa-bars' aria-hidden="true" style={{"fontSize":"24px"}} onClick={ShowMenu}/></div></div>
+            <div className='search-bar'  ref={menuRef}>
+                <div className='menu-icon'><div className='menu-icon2'> <i className='fa fa-bars' aria-hidden="true" style={{"fontSize":"24px"}} onClick={()=>(setShowMenu(!showmenu),setNewContact(false))} /></div></div>
                 <div className='frame-search'><div><i className="material-icons">search</i></div><input type="text" placeholder="Search" /></div>
                 {showmenu==true?
-               <div>
-                <button><span><span class="material-symbols-outlined">bookmark</span></span>Saved Cards</button>
-                <button onClick={ShowNewContact}><span><span class="material-symbols-outlined">person</span></span>New Contact</button>
-                <button><span><span class="material-symbols-outlined">supervisor_account</span></span>New Group</button>
-                <button><span><span class="material-symbols-outlined">logout</span></span>Logout</button>
+               <div ref={menuRef}>
+                    <button><span><span class="material-symbols-outlined">bookmark</span></span>Saved Cards</button>
+                    <button onClick={()=>(setNewContact(!newcontact),setShowMenu(false))}><span><span class="material-symbols-outlined">person</span></span>New Contact</button>
+                    <button><span><span class="material-symbols-outlined">supervisor_account</span></span>New Group</button>
+                    <button><span><span class="material-symbols-outlined">logout</span></span>Logout</button>
                 </div>                    
                 :null
                 }
@@ -252,22 +259,24 @@ function Chat(){
                     </NavLink>
                 </li>                    
             </ul>
-            {newcontact==true?
-                <div className='new_contact_add'>
-                    <div className='top'>
-                        <p>New Contact</p>
-                        <div>
-                            <input type="text" placeholder='First name'/>
-                            <input type="text" placeholder='Last name' />
-                            <input type="text" placeholder='Phone number'/>
+            <div>
+                {newcontact==true?
+                    <div className='new_contact_add' ref={menuRef}>
+                        <div className='top'>
+                            <p>New Contact</p>
+                            <div>
+                                <input type="text" placeholder='First name'/>
+                                <input type="text" placeholder='Last name' />
+                                <input type="text" placeholder='Phone number'/>
+                            </div>
                         </div>
-                    </div>
-                    <div className='button'>
-                        <button onClick={ShowNewContact}>Cancel</button>
-                        <button>Create Contact</button>
-                    </div>
-                </div> :null           
-            }
+                        <div className='button'>
+                            <button onClick={()=>setNewContact(false)}>Cancel</button>
+                            <button>Create Contact</button>
+                        </div>
+                    </div> :null           
+                }
+            </div>  
         </div>        
     )
 }
--- src/components/Top-bar.jsx
+++ src/components/Top-bar.jsx
@@ -2,15 +2,27 @@ import './Top-bar.css';
 import { NavLink } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
-import { useRef, useState } from 'react';
+import { useRef, useState,useEffect } from 'react';
 import { useContext } from 'react';
 import { Context } from './Context';
 
 function Topbar(){
     let [showchatmenu, setShowChatMenu] = useState(false)
-    function ShowChatMenu(){
-        setShowChatMenu(!showchatmenu)
-    }
+    let [deletechat,setDeleteChat] = useState(false)
+    let menuRef=useRef();
+    useEffect(()=>{
+        let handler=(e)=>{
+            if(!menuRef.current.contains(e.target)){
+                setShowChatMenu(false)
+                setDeleteChat(false)
+            }            
+        };
+        document.addEventListener("mousedown",handler)
+        return()=>{
+            document.removeEventListener("mousedown", handler)
+        }
+    });
+
     return(
         <div className='top-bar'>                   
             <NavLink className='other-user'>
@@ -23,17 +35,27 @@ function Topbar(){
             <NavLink>
                 <div><div><i className="material-icons">search</i></div></div>
                 <div><div><i className="material-icons" style={{"fontSize":"20px"}}>phone</i></div></div>
-                <div><div><i className='fa fa-ellipsis-v' onClick={ShowChatMenu}></i></div></div>                
+                <div><div><i className='fa fa-ellipsis-v' onClick={()=>(setShowChatMenu(!showchatmenu),setDeleteChat(false))}></i></div></div>                
             </NavLink>
-            {showchatmenu==true?
             <div>
-                <button><span><span class="material-symbols-outlined">videocam</span></span>Video Call</button>
-                <button><span><span class="material-symbols-outlined">mic</span></span>Mute notifications</button>
-                <button><span><span class="material-symbols-outlined">flag</span></span>Report</button>
-                <button><span><span class="material-symbols-outlined">delete</span></span>Delete chat</button>
-                <button><span><span class="material-symbols-outlined">block</span></span>Block</button>
+                {showchatmenu==true?
+                    <div ref={menuRef}>
+                        <button><span><span class="material-symbols-outlined">videocam</span></span>Video Call</button>
+                        <button><span><span class="material-symbols-outlined">mic</span></span>Mute notifications</button>
+                        <button><span><span class="material-symbols-outlined">flag</span></span>Report</button>
+                        <button onClick={()=>(!setDeleteChat(true),setShowChatMenu(false),console.log('yes'))}><span><span class="material-symbols-outlined">delete</span></span>Delete chat</button>
+                        <button><span><span class="material-symbols-outlined">block</span></span>Block</button>
+                    </div>
+                :null}                
             </div>
-            :null}
+            <div className='delete_chat_tab' >
+                {deletechat==true?
+                    <div ref={menuRef}>
+                        <div className='top'><p>Delete this chat?</p><p>Permanently delete the chat with ME Me?</p></div>
+                        <div className='buttom'><button onClick={()=>setShowChatMenu(false)}>CANCEL</button><button>DELETE CHAT</button></div>      
+                    </div>:null                       
+                }
+            </div>              
         </div>           
     )
 }
